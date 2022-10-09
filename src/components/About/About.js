cb2a5dd1f82ebe71198d8d4781d38c1b4f78c812
golammostofa58958 <golammostofa58958@gmail.com>
2022-10-09 12:52:20 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+
+const About = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h5>This is the true Real About me page</h5>
+        </div>
+    );
+};
+
+export default About;
